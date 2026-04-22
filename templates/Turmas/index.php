2ed1dd16a5bd7f34a54e20aa2431c59e63482da7
Luis Acosta <luiseduacosta@gmail.com>
2026-04-22 00:30:52 -0300

    Several fixes. Added turnos templaates. Change turna_estagios to turmas.

---
+++ templates/Turmas/index.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * @var \App\View\AppView $this
+ * @var \App\Model\Entity\Turma[]|\Cake\Collection\CollectionInterface $turmas
+ */
+declare(strict_types=1);
+
+$user_data = ['administrador_id' => 0, 'aluno_id' => 0, 'professor_id' => 0, 'supervisor_id' => 0, 'categoria' => '0'];
+$user_session = $this->request->getAttribute('identity');
+if ($user_session) {
+    $user_data = $user_session->getOriginalData();
+}
+?>
+
+<div class="container">
+
+    <?php if ($user_data['administrador_id']): ?>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light w-75 mx-auto" id="actions-sidebar">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler"
+                aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarToggler">
+                <ul class="navbar-nav ms-auto mt-lg-0">
+                    <li class="nav-item">
+                        <?= $this->Html->link(__('Nova turma de estágio'), ['action' => 'add'], ['class' => 'btn btn-primary float-end', 'style' => 'font-size: 10pt;']) ?>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    <?php endif; ?>
+
+    <?= $this->element('templates') ?>
+
+    <div class="row">
+        <h3><?= __('Turmas') ?></h3>
+    </div>
+
+    <div class="table-responsive">
+        <table class="table table-stripted table-hover table-responsive">
+            <thead>
+                <tr>
+                    <th><?= $this->Paginator->sort('id') ?></th>
+                    <th><?= $this->Paginator->sort('turma', 'Turma') ?></th>
+                    <th class="actions"><?= __('Ações') ?></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($turmas as $turma): ?>
+                    <tr>
+                        <td><?= $turma->id ?></td>
+                        <td><?= h($turma->turma) ?></td>
+                        <td class="actions">
+                            <?= $this->Html->link(__('Ver'), ['action' => 'view', $turma->id]) ?>
+                            <?php if ($user_data['administrador_id']): ?>
+                                <?= $this->Html->link(__('Editar'), ['action' => 'edit', $turma->id]) ?>
+                                <?= $this->Form->postLink(__('Excluir'), ['action' => 'delete', $turma->id], ['confirm' => __('Tem certeza que quer excluir este registro # {0}?', $turma->id)]) ?>
+                            <?php endif; ?>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+
+    <?= $this->element('templates'); ?>
+
+    <div class="d-flex justify-content-center">
+        <div class="paginator">
+            <ul class="pagination">
+                <?= $this->element('paginator') ?>
+            </ul>
+        </div>
+        <?= $this->element('paginator_count') ?>
+    </div>
